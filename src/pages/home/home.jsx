d770f1953d56279ded15e32292e718d6136a0d38
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-10-20 22:33:30 -0300

    feat: update home components to consume API

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../components/card/card";
+
+import { api } from "../../utils/api/api";
 
 import "./home.css";
 
 export function Home() {
+  const [animeList, setAnimeList] = useState([]);
+
+  async function getAnimes() {
+    const animes = await api.getAllAnimes();
+    setAnimeList(animes);
+  }
+
+  useEffect(() => {
+    getAnimes();
+  });
+
   return (
     <>
       <div className="card-list">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {animeList.map((item, index) => {
+          return <Card key={index} />;
+        })}
       </div>
     </>
   );
